b09d01839bd432b8a563dd53da4383af9fd6e037
heraldofortuna <heraldofortuna@gmail.com>
2022-04-07 11:00:28 -0500

    Se consumió el api hello que viene con la aplicación nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div className={styles.container}>
       <Head>
